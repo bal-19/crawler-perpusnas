32bf38dc2aecea0f75f0ae117638406b30422fe2
bal-19 <iqbalhaidee19@gmail.com>
2024-10-18 18:48:21 +0700

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import argparse
+from src import Perpusnas
 
 def main():
-    parser = argparse.ArgumentParser(description="Engine untuk mengambil data perpustkaan")
-    parser.add_argument("--insert", action="store_true", help="Insert data to MongoDB")
-    args = parser.parse_args()
-
-    
-    if args.insert:
-        print("Data successfully inserted to MongoDB")
+    Perpusnas().run()
 
 if __name__ == "__main__":
     main()
